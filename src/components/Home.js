3346f8806fc41dd7b854429821bdf1880802a64e
calee14 <caplee1000@gmail.com>
2022-04-30 02:29:56 -0700

    add logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useMoralis } from "react-moralis"
 
 export const Home = () => {
+
+    const { logout, isAuthenticated, isAuthUndefined, authError } = useMoralis();
+
     return (
-        <Box>Home</Box>
+        <Box>
+            {isAuthenticated && 
+                <Button onClick={() => logout()}>Logout</Button>
+            }
+        </Box>
     );
 }
