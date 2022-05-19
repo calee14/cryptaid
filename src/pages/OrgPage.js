3432ffc2b3f79d891345aa48dd80b7f518ea3204
Gen Tamada <gtamada@ucsb.edu>
2022-05-19 00:30:11 -0700

    Added some edit function for user who own the organization
    Still need to fix checkbox of milestone
    Add links variable
    etc.

--- src/pages/OrgPage.js
+++ src/pages/OrgPage.js
@@ -1,10 +1,11 @@
 import { CheckCircleIcon, SettingsIcon } from "@chakra-ui/icons";
-import { Box, Button, Center, Divider, Heading, Image, Spacer, Text, Flex, Progress, List, ListItem, ListIcon, Grid, GridItem, Container } from "@chakra-ui/react";
+import { Box, Button, Input, InputRightElement, InputGroup, Center, Divider, Heading, Image, Checkbox,  Stack, NumberInput, NumberInputField, Spacer, Text, Flex, Progress, List, ListItem, ListIcon, Grid, GridItem, Container } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router";
 import { useOrgData } from "../hooks/useOrgData";
 import { NftCard } from "../components/NftCard";
-import { EditIcon } from '@chakra-ui/icons'
+import { EditIcon, CloseIcon } from '@chakra-ui/icons'
+import { useState } from "react";
 import Moralis from "moralis";
 
 export const OrgPage = () => {
@@ -12,6 +13,21 @@ export const OrgPage = () => {
     const org_id = params.id;
     const navigate = useNavigate();
     const org_data = useOrgData(org_id)
+    const checkIfOwner = (Moralis.User?.current()?.id === org_data.owner)
+    const [editMode, setEditMode] = useState(false)
+    const currentDate = new Date()
+
+    const [numMilestone, setNumMilestone] = useState(org_data.milestone.length);
+
+    const [completeSwitch, setCompleteSwitch] = useState([false,false,false,false,false])
+
+    const changeSwitch = (index) => {
+        let array = completeSwitch
+        array[index] = !array[index]
+        console.log(completeSwitch[4])
+        setCompleteSwitch(array)
+    }
+
     
     return (
         <Box mx={'10%'}>
@@ -19,43 +35,92 @@ export const OrgPage = () => {
             <Flex width={"100%"}>
                 <Box flex={2} paddingX={"3rem"}>
                     {/* organiztaion's title, img, progress, and donate button */}
+                    
                     <Flex>
-                    <Heading>{org_data.title}</Heading>
+                    {editMode?<Input defaultValue={org_data.title}/>:<Heading>{org_data.title}</Heading>}
                     <Spacer/>
-                    {Moralis.User?.current()?.id === org_data.owner?<Button leftIcon={<EditIcon />} colorScheme='teal' variant='solid'>
+                    {checkIfOwner && !editMode?<Button leftIcon={<EditIcon />} colorScheme='teal' onClick={() => setEditMode(true)} variant='solid'>
                         Edit
                     </Button>: ""}
+                    {editMode?<Button leftIcon={<CloseIcon />} colorScheme='red' onClick={() => setEditMode(false)} variant='solid'>
+                        Cancel
+                    </Button>: ""}
                     </Flex>
+
+
                     <Spacer my={3}/>
                     <Text fontSize={"lg"} fontWeight={"semibold"}>Raised {org_data.donated} ETH out of {org_data.goal} ETH</Text>
                     <Spacer my={3}/>
                     <Progress height={1} value={org_data.donated/org_data.goal*100} rounded="md" size={"sm"}></Progress>
                     <Spacer my={3}/>
+                    
+                    
                     {/* donate button will route to donate page passing the org id */}
-                    {Moralis.User?.current()?.id? <Button width={"50%"} onClick={() => navigate("/organization/" + org_id + "/donate", { state: { org_id: org_id} })}>
+                    {Moralis.User?.current()?.id? 
+                        <Button width={"50%"} onClick={() => navigate("/organization/" + org_id + "/donate", { state: { org_id: org_id} })}>
                         Donate Now
-                    </Button>: <Button width={"50%"}>Login to Donate</Button>}
+                        </Button>: 
+                        <Button width={"50%"}>Login to Donate</Button>
+                    }
+
+                    
                     <Spacer my={3}/>
-                    <Text fontWeight={"medium"} >Deadline: <b>{org_data.deadline}</b></Text>
+                    {editMode? 
+                    <Stack shouldWrapChildren direction='row'>
+                        <Text fontSize={"2xl"} >Deadline: </Text>
+                        <NumberInput maxW={20} defaultValue={org_data.deadline.getMonth()+1} min={1} max={12}>
+                            <NumberInputField />
+                        </NumberInput>
+
+                        <NumberInput maxW={20} defaultValue={org_data.deadline.getDate()} min={1} max={31}>
+                            <NumberInputField />
+                        </NumberInput>
+
+                        <NumberInput maxW={32} defaultValue={org_data.deadline.getFullYear()} min={currentDate.getFullYear()} >
+                            <NumberInputField />
+                        </NumberInput>
+                    </Stack>: ""}
+                    {(org_data.deadline?.getTime() === 0) || editMode? "":<Text fontWeight={"medium"} >Deadline: <b>{org_data.deadline?.getMonth()+1}/{org_data.deadline?.getDate()}/{org_data.deadline.getFullYear()}</b></Text>}
+                    
+
+
                     <Spacer my={3}/>
                     <Image position={"relative"} width={"100%"} objectFit={"cover"} height={"26rem"} src={org_data.imgUrl} rounded={"xl"} alt='Org Image here'/>
                     {/* organization description */}
                     <Center height={5} >
                         <Divider orientation="horizontal"/>
                     </Center>
-                    <Text fontWeight={"medium"}>{org_data.description}
-                    </Text>
+
+                    
+                    {editMode? <Input defaultValue={org_data.description}/> : <Text fontWeight={"medium"}>{org_data.description}</Text>}
                     {/* organization milstones */}
                     <Center height={5} >
                         <Divider orientation="horizontal"/>
                     </Center>
+                    {editMode && numMilestone===5?<Button isDisabled={true}>Add Milestone</Button>:""}
+                    {editMode && numMilestone<5?<Button onClick={() => setNumMilestone(numMilestone+1)}>Add Milestone</Button>: ""}
+                    {editMode && numMilestone>0?<Button onClick={() => setNumMilestone(numMilestone-1)}>Remove Milestone</Button>: ""}
+                    {editMode && numMilestone>=1?<Input placeholder="Add milestone here" defaultValue={org_data.milestone[0]?org_data.milestone[0]:"" }/>:""}
+                    {editMode && numMilestone>=2?<Input placeholder="Add milestone here" defaultValue={org_data.milestone[1]?org_data.milestone[1]:""}/>:""}
+                    {editMode && numMilestone>=3?<Input placeholder="Add milestone here" defaultValue={org_data.milestone[2]?org_data.milestone[2]:""}/>:""}
+                    {editMode && numMilestone>=4?<Input placeholder="Add milestone here" defaultValue={org_data.milestone[3]?org_data.milestone[3]:""}/>:""}
+                    {editMode && numMilestone>=5?<InputGroup>
+                        <Input
+                            defaultValue={org_data.milestone[4]?org_data.milestone[4]:""}
+                            placeholder="Add milestone here"
+                        />                        
+                        <InputRightElement>
+                            <Checkbox onChange = {() =>changeSwitch(4)}/> {/*Fix default value of checkbox*/}
+                        </InputRightElement>
+                        </InputGroup> 
+                    :""}
                     <Heading fontSize="lg">Milestones:</Heading>
                     <Spacer my={2}/>
                     <List spacing={3}>
                         {org_data.milestone.map((ms, i) => {
                             return (
                             <ListItem>
-                                <ListIcon as={org_data.progress[i] === 100 ? CheckCircleIcon : SettingsIcon} color={org_data.progress[i] === 100 ? "green.500" : "gray.600"}/>
+                                <ListIcon as={org_data.progress[i] ? CheckCircleIcon : SettingsIcon} color={org_data.progress[i]? "green.500" : "gray.600"}/>
                                 {ms}
                             </ListItem>
                             );
