8c32b0f8ab4dddde37253fd4fa3118e9d07033ea
calee14 <caplee1000@gmail.com>
2022-05-21 02:00:48 -0700

    rarible is broken af, movin to smart contract

--- src/hooks/useMintNft.js
+++ src/hooks/useMintNft.js
@@ -1,56 +1,69 @@
 import { Moralis } from "moralis";
 import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
+import Rarepress from 'rarepress.js';
+import Rareterm from 'rareterm';
 
 export const useMintNft = () => {
     
     const { enableWeb3, isWeb3Enabled, web3 } = useMoralis();
+    
 
     useEffect(() => {
-        Moralis.initPlugins();
+        async function initalize() {
+            await Moralis.initPlugins(); 
+        }
+
+        initalize();
 
         if (!isWeb3Enabled) {
             enableWeb3();
         }
+        
     }, [web3, enableWeb3, isWeb3Enabled]);
 
     return async function(_owner, _name, _description, _imgData, _supply) {
 
-        const imgFile = new Moralis.File(_imgData.name, _imgData);
-        await imgFile.saveIPFS();
 
-        const imgHash = imgFile.hash();
+        // const imgFile = new Moralis.File(_imgData.name, _imgData);
+        // await imgFile.saveIPFS();
 
-        console.log(_owner);
-        console.log(imgHash);
-        console.log(imgFile.ipfs());
-        console.log('/ipfs/'+imgHash);
+        // const imgHash = imgFile.hash();
 
-        const metadata = {
-            name: _name,
-            description: _description,
-            image: "/ipfs/" + imgHash
-        };
+        // console.log(_owner);
+        // console.log(imgHash);
+        // console.log(imgFile.ipfs());
+        // console.log('/ipfs/'+imgHash);
+
+        // const metadata = {
+        //     name: _name,
+        //     description: _description,
+        //     image: "/ipfs/" + imgHash,
+        // };
         
-        const metaFile = new Moralis.File("metadata.json", {base64: btoa(JSON.stringify(metadata))});
-        await metaFile.saveIPFS();
+        // const metaFile = new Moralis.File("metadata.json", {base64: btoa(JSON.stringify(metadata))});
+        // await metaFile.saveIPFS();
 
-        const metaDataHash = metaFile.hash();
-        console.log(metaFile.ipfs());
-        
-        let res = await Moralis.Plugins.rarible.lazyMint({
-            chain: 'rinkeby',
-            userAddress: _owner,
-            tokenType: 'ERC721',
-            tokenUri: '/ipfs/' + metaDataHash,
-            supply: 1,
-            royaltiesAmount: 1,
-            list: true, // if lazy listing
-            listTokenAmount: 1,
-            listTokenValue: 10**18, // 1 Eth
-            listAssetClass: 'ETH',
+        // const metaDataHash = metaFile.hash();
+        // console.log(metaFile.ipfs());
+        const rarepress = new Rareterm();
+
+        await rarepress.init({ host: "https://rinkeby.rarenet.app/v1"}).then(async (address) => {
+            console.log(address);
+            let token = await rarepress.token.create({
+                type: "ERC721",
+                metadata: {
+                    name: "Title goes here",
+                    description: "Description goes here",
+                    image: "/ipfs/" + 'hello',
+                }
+            });
+            
+            console.log(token);
+            // // publish the token itself to Rarible marketplace
+            let receipt = await rarepress.token.send(token);
+            console.log(receipt);
+            console.log('finished');
         });
-
-        // console.log(res);
     };
 };
--- src/pages/MintNft.js
+++ src/pages/MintNft.js
@@ -14,19 +14,11 @@ export const MintNft = () => {
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
     const navigate = useNavigate();
-    // const mint = useMintNft();
+    const mint = useMintNft();
     
     const org_data = useOrgData(org_id);
 
     const { enableWeb3, isWeb3Enabled, web3 } = useMoralis();
-
-    useEffect(() => {
-        Moralis.initPlugins();
-
-        if (!isWeb3Enabled) {
-            enableWeb3();
-        }
-    }, [web3, enableWeb3, isWeb3Enabled]);
 
     const selectHandler = (event) => {
         setSelectedFile(event.target.files[0]);
@@ -35,35 +27,8 @@ export const MintNft = () => {
 
     const handleMint = async (e) => {
         e.preventDefault();
-        const imgFile = new Moralis.File(selectedFile.name, selectedFile);
-        await imgFile.saveIPFS();
-
-        const imgHash = imgFile.hash();
-
-        console.log(org_data.ethAddress);
-        console.log(imgHash);
-        console.log(imgFile.ipfs());
-        console.log('/ipfs/'+imgHash);
-
-        const metadata = {
-            name: 'name',
-            description: 'description',
-            image: "/ipfs/" + imgHash
-        };
         
-        const metaFile = new Moralis.File("metadata.json", {base64: btoa(JSON.stringify(metadata))});
-        await metaFile.saveIPFS();
-
-        const metaDataHash = metaFile.hash();
-        console.log(metaFile.ipfs());
-        
-        let res = await Moralis.Plugins.rarible.lazyMint({
-            chain: 'rinkeby',
-            userAddress: '0x7f64041298CC2C045FE5eb0e897ab7b5D4BdB4F3',
-            tokenType: 'ERC721',
-            tokenUri: '/ipfs/' + metaDataHash,
-        });
-
+        await mint(org_data.ethAddress, 'name', 'desc', selectedFile, 1);
     }
 
     return (
