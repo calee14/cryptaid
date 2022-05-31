2ee9553c8360f7f7c155a69fa645e492b633271a
calee14 <caplee1000@gmail.com>
2022-05-31 00:10:12 -0700

    transact with smart contract to swap

--- src/hooks/useSwapContract.js
+++ src/hooks/useSwapContract.js
@@ -9,14 +9,22 @@ export const useSwapContract = (_units, _receiver) => {
     
     const { enableWeb3, isWeb3Enabled, web3, user } = useMoralis();
     
-    return async function(tokenAddress) {
+    return async function(amountEthIn, tokenAddress) {
         await enableWeb3();
+        
+        const amountIn = Moralis.Units.ETH(amountEthIn);
         const web3 = new Web3(Moralis.provider);
         const contract = new web3.eth.Contract(contractABI, contractAddress);
-        const swapTransaction = contract.methods.buyToken(tokenAddress);
+        const estimateConversion = contract.methods.getEstimatedETHforToken(amountIn, tokenAddress);
 
-        const response = await swapTransaction.send({ from: user.get('ethAddress')});
+        const response = await estimateConversion.call();
 
-        return response;
+        const newTokenAmount = response[0];
+
+        const swapTransaction = contract.methods.convertEthToToken(newTokenAmount, tokenAddress);
+        const swapResponse = await swapTransaction.send({from: user.get('ethAddress'), value: amountIn})
+        // https://web3js.readthedocs.io/en/v1.2.11/web3-eth.html#eth-sendtransaction how to send transaction with value
+
+        return swapResponse;
     }
 };
