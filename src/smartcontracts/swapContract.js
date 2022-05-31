562038b60a621bf476b0990371512c9119adbe1e
calee14 <caplee1000@gmail.com>
2022-05-30 23:22:02 -0700

    add contract abi

--- src/smartcontracts/swapContract.js
+++ src/smartcontracts/swapContract.js
@@ -1,10 +1,75 @@
-const contractAddress = "put address here";
+const contractAddress = "0x42273E61205f6Aab6019De53362661C57C2B6142";
 
 /**
  * Swap contract ABI here
  * temporary one here until updated
  */
 
-const contractABI = []
+const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "daiAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "convertEthToDai",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "daiAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "getEstimatedETHforToken",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "uniswapRouter",
+		"outputs": [
+			{
+				"internalType": "contract IUniswapV2Router02",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 export { contractABI, contractAddress }; 
