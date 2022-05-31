ed5d6fc8ef289c9e5c4303c198ce3ea828a043bc
calee14 <caplee1000@gmail.com>
2022-05-31 00:10:23 -0700

    ui to swap token

--- src/pages/ChangeToken.js
+++ src/pages/ChangeToken.js
@@ -1,19 +1,26 @@
 import { Heading, Box, Button, NumberInput, NumberInputField, Spacer, InputGroup, Text, InputRightElement, Input } from "@chakra-ui/react"
 import "../smartcontracts/TokenSwap.sol"
 import { useState } from "react";
-// import { useSwapContract } from '../hooks/useSwapContract.js';
+import { useSwapContract } from '../hooks/useSwapContract.js';
 
 export const ChangeToken = () => {
     const [amount, setAmount] = useState(0.0);
     const [tokenAddress, setTokenAddress] = useState('');
+    const swap = useSwapContract();
+    const [swapping, setSwapping] = useState(false);
+    const [transactionMessage, setMessage] = useState();
 
-    const handleSwap = () => {
-
+    const handleSwap = async () => {
+        setSwapping(true);
+        const res = await swap(amount, tokenAddress);
+        console.log(res);
+        setMessage('Swap complete! Block hash: ' + res.blockHash);
+        setSwapping(false);
     }
 
     return(
         <Box mx={"14%"}>
-            <Heading>Swap tokens! Secure your Defi!!</Heading>
+            <Heading>Swap tokens! Control your Defi!!</Heading>
             <Spacer my={5}/>
             <Text fontSize={'xl'}>Amount</Text>
             <InputGroup>
@@ -22,7 +29,7 @@ export const ChangeToken = () => {
                         pointerEvents='none'
                         children={"Eth"}
                     />
-                    <NumberInputField defaultValue={amount}/>
+                    <NumberInputField  onChange={(e) => setAmount(e.currentTarget.value)}/>
                 </NumberInput>
             </InputGroup>
             <Text fontSize={'xl'}>Token</Text>
@@ -30,7 +37,13 @@ export const ChangeToken = () => {
                 <Input value={tokenAddress} onChange={(event) => setTokenAddress(event.currentTarget.value)} placeholder="Your token address"/>
             </InputGroup>
             <Spacer my={5}/>
-            <Button onClick={() => handleSwap()}>Token Swap</Button>
+            {transactionMessage && 
+                <>
+                    <Text color={'green.400'} fontSize={'sm'}>{transactionMessage}</Text>
+                    <Spacer my={2}/>
+                </>
+            }
+            <Button isLoading={swapping} onClick={() => handleSwap()}>Token Swap</Button>
             
         </Box>
     )
